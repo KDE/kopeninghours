f5241f29a24e4ec6734745d8224e57f130fb6213
Volker Krause <vkrause@kde.org>
2020-12-04 20:22:05 +0100

    Add simple validation test
    This can read expressions from stdin, allowing to run this against the
    complete OSM corpus of real-world expressions. Takes about four seconds
    for 493k expressions, about 30k of those get flagged as invalid.
    
    Good news though, no crash and no memory access issues :)

---
+++ tests/osm-opening-hours-validate.cpp
@@ -0,0 +1,53 @@
+/*
+    SPDX-FileCopyrightText: 2020 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <KOpeningHours/OpeningHours>
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QFile>
+
+#include <iostream>
+
+using namespace KOpeningHours;
+
+int main(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument(QStringLiteral("expression"), QStringLiteral("OSM opening hours expression, omit for using stdin."));
+    parser.process(app);
+
+    if (parser.positionalArguments().isEmpty()) {
+        QFile in;
+        in.open(stdin, QFile::ReadOnly);
+        int total = 0;
+        int errors = 0;
+        while (!in.atEnd()) {
+            auto expr = in.readLine();
+            expr.replace('\n', "");
+            expr.replace('\r', "");
+            if (expr.isEmpty()) {
+                continue;
+            }
+            ++total;
+            OpeningHours oh(expr);
+            if (oh.error() == OpeningHours::SyntaxError) {
+                std::cerr << "Syntax error: " << expr.constData() << std::endl;
+                ++errors;
+            }
+        }
+
+        std::cerr << total << " expressions checked, " << errors << " invalid" << std::endl;
+        return errors;
+    } else {
+        OpeningHours oh(parser.positionalArguments().at(0).toUtf8());
+        return oh.error() != OpeningHours::SyntaxError ? 0 : 1;
+    }
+}
